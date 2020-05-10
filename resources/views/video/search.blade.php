8e1be69c006d5286fce903d0207f403647114b46
mcespede <cespedes.mauro@gmail.com>
2020-05-10 21:17:39 +0200

    Esta es una vista nueva donde exporte toda la estructura de  busqueda Para ello utilizo un @include Separe en partes la vista de  HOME para que sea modular Asi se hace mas facil de trabajar

--- resources/views/video/search.blade.php
+++ resources/views/video/search.blade.php
@@ -1,1 +1,17 @@
-@include('home')
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+
+        <!----------------------------------------->
+        <div class="container">
+        	<!--Vamos a saber lo que estamos buscando--->
+			<h2>Busqueda {{$search}}</h2>
+
+            @include('video.videosList')
+        </div>
+        <!----------------------------------------->
+    </div>
+</div>
+@endsection
