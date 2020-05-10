e550f6fcda2f43caae44e96902d8f00b86b9f32a
mcespede <cespedes.mauro@gmail.com>
2020-05-10 19:21:42 +0200

    resources/views/layouts/app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,12 +64,13 @@
                         &nbsp;
                     </ul>
                     <!-- -------------SEARCH-BAR ------------------>
-                    <form class="navbar-form navbar-left" role="search" action="">
+                    <!-- Aqui añadimos la ruta de buscar para funcione con el controlador.-->
+                    <form class="navbar-form navbar-left" role="search" action="{{url('/buscar')}}">
                         <div class="form-group">
                             <input type="text" class="form-control" placeholder="Que quieres ver?" name="search">
                         </div>
-                        <button type="button" class="btn btn-info">
-                                <span class="glyphicon glyphicon-search"></span> Search
+                        <button type="submit" class="btn btn-success">
+                            <span class="glyphicon glyphicon-search"></span>
                         </button>
                     </form>
                     <!-- -------------/SEARCH-BAR ------------------>
