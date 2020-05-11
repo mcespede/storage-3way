0523cc650d7cda083d08fe0cb339e17c38458049
mcespede <cespedes.mauro@gmail.com>
2020-05-11 19:43:34 +0200

    Nueva vista de  SEARCH Despues de agregar la barra de busqueda en el menu lateral reacomode los elementos de la pagina de busqueda para que se despliegen correntamente.

--- resources/views/video/search.blade.php
+++ resources/views/video/search.blade.php
@@ -5,32 +5,43 @@
     <div class="row">
 
         <!----------------------------------------->
-      	<div class="container">
-      			<div class="col-md-4">
-   	         		<!--Vamos a saber lo que estamos buscando--->
-					<h2>Busqueda {{$search}}</h2>     				
-      			</div><br/>
-	
 
-			<!-- Esta es una forma de poder ordenar y escojer que deseo seleccionar
+		<div class="col-md-8">
+			<div class="row">
+				<div class="col-md-2">
+					<!--Vamos a saber lo que estamos buscando--->
+					<h2>Busqueda {{$search}}</h2>   					
+				</div>
+
+
+  				<div class="col-md-10">
+  					<!-- Esta es una forma de poder ordenar y escojer que deseo seleccionar
 				LE concatenamos el termino de la busqueda-->
-			<div class="col-md-8">
-				<form class="col-md-4 pull-right" action="{{url('/buscar/'.$search)}}" method="get">
-					<label for="filter">Ordenar</label>
-					<!-- FIlter es como se va a llamar el parametro que vamos a enviar al controlador-->
-					<select name="filter" class="form-control">
-						<option value="new">Mas nuevos primero</option>
-						<option value="old">Mas antiguos primero</option>
-						<option value="alfa">De la A a la Z</option>
-					</select>
-					<!-- Agrego la clase BTN-FILTER para poder separa un poco el boton del filtro-->
-					<input type="submit" value="Ordenar" class="filter btn btn-sm btn-primary">
-				</form>	        
+					<form class="col-md-4 pull-right" action="{{url('/buscar/'.$search)}}" method="get">
+						<label for="filter">Ordenar</label>
+						<!-- FIlter es como se va a llamar el parametro que vamos a enviar al controlador-->
+						<select name="filter" class="form-control">
+							<option value="new">Mas nuevos primero</option>
+							<option value="old">Mas antiguos primero</option>
+							<option value="alfa">De la A a la Z</option>
+						</select>
+							<!-- Agrego la clase BTN-FILTER para poder separa un poco el boton del filtro-->
+						<input type="submit" value="Ordenar" class="filter btn btn-sm btn-primary">
+					</form>	
+  				</div>	
+
 			</div>
 
+
 			<div class="clearfix"></div>
-	        @include('video.videosList')
-	    </div>
+	        @include('video.videosList')       
+		</div>
+
+
+
+	     <div class="col-md-4">
+          @include('layouts.sideMenu')
+        </div> 
         <!----------------------------------------->     		
 
 
